127a6c8a7db7c944a52a00070128b45feb4348b8
Muji <zxc215236qq@gmail.com>
2026-06-15 16:48:59 +0800

    feat: add drop item

---
+++ Assets/Script/Drop/DropData.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "DropData", menuName = "Drop/DropData")]
+public class DropData : ScriptableObject
+{
+    public E_DropType dropType;
+    public int amount;
+
+    public float attractRange;
+    public float moveSpeed;
+}
+
+public enum E_DropType
+{
+    Coin,
+    Exp,
+}
---
+++ Assets/Script/Drop/DropItem.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+
+public class DropItem : MonoBehaviour
+{
+    [SerializeField] private DropData data;
+    [SerializeField] private LayerMask playerLayer;
+    private Transform targetPlayer;
+    private bool isAttracking;
+
+    void Update()
+    {
+        FindPlayerInRange();
+        if (isAttracking && targetPlayer != null)
+        {
+            MoveToPlayer();
+        }
+    }
+
+    private void FindPlayerInRange()
+    {
+        if (isAttracking) return;
+
+        Collider[] hits = Physics.OverlapSphere(transform.position, data.attractRange, playerLayer);
+        if(hits.Length > 0 )
+        {
+            targetPlayer = hits[0].transform;
+            isAttracking = true;
+        }
+    }
+
+    private void MoveToPlayer()
+    {
+        Vector3 dir = (targetPlayer.position - transform.position).normalized;
+        transform.position += dir * data.moveSpeed * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        PlayerProgression player = other.GetComponent<PlayerProgression>();
+        if (player == null) return;
+
+        switch (data.dropType)
+        {
+            case E_DropType.Coin:
+                player.AddCoin(data.amount);
+                break;
+            case E_DropType.Exp:
+                player.AddExp(data.amount);
+                break;
+        }
+
+        Destroy(gameObject);
+    }
+}
